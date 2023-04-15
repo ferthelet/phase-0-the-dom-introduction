0ca98fcf05958065da502dc61a53f580dbdff261
Juan Salazar <ferthelet@gmail.com>
2023-04-14 22:58:51 -0500

    adds exercise files

---
+++ index.js
@@ -0,0 +1,14 @@
+// js
+
+// window is where everything else lives
+let varToPrint = window.innerHeight;
+
+// document
+varToPrint = document.URL;
+
+// queries
+varToPrint = document.querySelector("meta");
+
+// print
+console.log(varToPrint)
+
